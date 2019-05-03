cb491f1afa6df41c5c0afbc6485e95bcdb19b995
derrickfox <fox.derrick@gmail.com>
2019-05-03 17:34:14 -0400

    generated sources components

---
+++ src/app/lists/sources/source-detail/source-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-source-detail',
+  templateUrl: './source-detail.component.html',
+  styleUrls: ['./source-detail.component.css']
+})
+export class SourceDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
